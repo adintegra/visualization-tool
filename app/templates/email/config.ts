bf8b74a1d3073edfa15dea4e5d0a2753e039f206
Mark Kunzmann <mark.kunzmann@adintegra.com>
2025-11-28 12:00:12 +0100

    fix: Cleanup

--- app/templates/email/config.ts
+++ app/templates/email/config.ts
@@ -1,2 +1,2 @@
-export const OWNER_ORGANIZATION_EMAIL = "visualize@bafu.admin.ch";
+export const OWNER_ORGANIZATION_EMAIL = "no-reply@example.com";
 export const SUPPORT_EMAIL = "no-reply@example.com";
